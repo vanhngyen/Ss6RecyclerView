88807816b96fd0c54f7b604708e1d679c526ff24
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2021-06-04 20:05:24 +0700

    ss06

---
+++ app/src/main/java/com/example/ss6recyclerview/adapter/ContactAdapter.java
@@ -0,0 +1,28 @@
+package com.example.ss6recyclerview.adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ContactAdapter extends RecyclerView.Adapter {
+
+
+
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder( RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/ss6recyclerview/model/Contact.java
@@ -0,0 +1,42 @@
+package com.example.ss6recyclerview.model;
+
+
+//java:POJO C#:
+public class Contact {
+    private String name;
+    private String phone;
+    private int avatar;
+
+    public Contact() {
+    }
+
+    public Contact(String name, String phone, int avatar) {
+        this.name = name;
+        this.phone = phone;
+        this.avatar = avatar;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(int avatar) {
+        this.avatar = avatar;
+    }
+}
